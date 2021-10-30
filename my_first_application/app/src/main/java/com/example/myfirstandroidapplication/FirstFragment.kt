f654800f1e0069b6a744bd432af1fa874d871fdd
Kotaro Kamashima <kotaro0726@gmail.com>
2021-10-30 12:03:22 +0900

    Update: Display toast text

--- my_first_application/app/src/main/java/com/example/myfirstandroidapplication/FirstFragment.kt
+++ my_first_application/app/src/main/java/com/example/myfirstandroidapplication/FirstFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.myfirstandroidapplication.databinding.FragmentFirstBinding
 
@@ -32,9 +34,15 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.randomButton.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        view.findViewById<Button>(R.id.toast_button).setOnClickListener {
+            val myToast = Toast.makeText(context, "Hello Toast!", Toast.LENGTH_SHORT);
+
+            myToast.show();
         }
+
+//        binding.randomButton.setOnClickListener {
+//            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+//        }
     }
 
     override fun onDestroyView() {
