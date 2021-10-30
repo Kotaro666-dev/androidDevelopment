9adcf2dc358290ffda059dfde4efc8c90b081782
Kotaro Kamashima <kotaro0726@gmail.com>
2021-10-30 14:18:37 +0900

    Update: Finish task

---
+++ AndroidKotlinFundamentals/MyDiceRoller/app/src/main/java/com/example/mydiceroller/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.mydiceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.isDigitsOnly
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener { rollDice() }
+
+        val countUpButton: Button = findViewById(R.id.count_up_button)
+        countUpButton.setOnClickListener { countUp() }
+    }
+
+    private fun rollDice() {
+//        Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        val resultText: TextView = findViewById(R.id.result_text)
+        val randomInt = (1..6).random()
+        resultText.text = randomInt.toString()
+    }
+
+    private fun countUp() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        if (!resultText.text.isDigitsOnly()) {
+            resultText.text = "1"
+            return
+        }
+        // The text value in resultText.text is an instance of the CharSequence class;
+        // it needs to be converted to a String object before it can be converted to an int.
+        var currentNumber: Int = resultText.text.toString().toInt()
+        if (6 <= currentNumber) {
+            return
+        }
+        currentNumber++;
+        resultText.text = currentNumber.toString()
+    }
+}
