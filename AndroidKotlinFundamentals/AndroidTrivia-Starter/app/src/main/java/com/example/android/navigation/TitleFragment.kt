633aff780036a63c73397f406894259584ebedf1
Kotaro Kamashima <kotaro0726@gmail.com>
2021-10-31 19:17:05 +0900

    Update: Add options on action bar

--- AndroidKotlinFundamentals/AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ AndroidKotlinFundamentals/AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -1,12 +1,11 @@
 package com.example.android.navigation
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 // TODO: Rename parameter arguments, choose names that match
@@ -26,8 +25,17 @@ class TitleFragment : Fragment() {
             R.layout.fragment_title,container,false)
 
         binding.playButton.setOnClickListener { view: View -> view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)}
-
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController()) || super.onOptionsItemSelected(item)
     }
 
     companion object {
