f3816fd1a37169b949e19f868ed9d26d93a3a08c
Kotaro Kamashima <kotaro0726@gmail.com>
2021-10-31 20:29:15 +0900

    Update: Add safeArgs and debug codes

--- AndroidKotlinFundamentals/ExternalActivities/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ AndroidKotlinFundamentals/ExternalActivities/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -20,6 +20,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -33,6 +34,10 @@ class GameWonFragment : Fragment() {
         val binding: FragmentGameWonBinding = DataBindingUtil.inflate(
                 inflater, R.layout.fragment_game_won, container, false)
         binding.nextMatchButton.setOnClickListener { view: View -> view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment) }
+
+        val args = GameWonFragmentArgs.fromBundle(requireArguments())
+        Toast.makeText(context, "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}", Toast.LENGTH_LONG).show()
+
         return binding.root
     }
 }
