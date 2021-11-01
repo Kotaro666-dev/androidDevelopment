da47227a912bb5795c1cbef9e8c18b78d1df97ed
Kotaro Kamashima <kotaro0726@gmail.com>
2021-11-01 09:32:05 +0900

    Uopdate: Add onCreate and onStart lifescycles and add timber library

--- AndroidKotlinFundamentals/DessertClicker-Starter/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ AndroidKotlinFundamentals/DessertClicker-Starter/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -18,6 +18,7 @@ package com.example.android.dessertclicker
 
 import android.content.ActivityNotFoundException
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
@@ -64,6 +65,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        Log.i("MainActivity", "onCreate Called")
+
         // Use Data Binding to get reference to the views
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
@@ -93,6 +96,11 @@ class MainActivity : AppCompatActivity() {
 
         // Show the next dessert
         showCurrentDessert()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MainActivity", "onStart Called")
     }
 
     /**
