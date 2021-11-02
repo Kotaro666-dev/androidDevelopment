cb6e2b613322818f7998e359ee1050f96992a4fd
Kotaro Kamashima <kotaro0726@gmail.com>
2021-11-02 21:57:36 +0900

    Update: Add basic alert dialog

--- dialogs/app/src/main/java/com/example/dialogs/MainActivity.kt
+++ dialogs/app/src/main/java/com/example/dialogs/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.dialogs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.dialogs.databinding.ActivityMainBinding
+import com.example.dialogs.dialogs.BasicAlertDialogClass
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        binding.basicDialogButton.setOnClickListener { displayBasicAlertDialog() }
+
+        setContentView(view)
+    }
+
+    private fun displayBasicAlertDialog() {
+        val basicAlertDialog = BasicAlertDialogClass()
+        basicAlertDialog.show(supportFragmentManager, "Basic Alert Dialog")
     }
 }
