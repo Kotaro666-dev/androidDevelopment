2b627e94a18bdaa2fb93f18f655e1781616e12fb
Kotaro Kamashima <kotaro0726@gmail.com>
2021-11-03 17:56:34 +0900

    Update: Add animation

--- jetpackComposePractice/JetpackComposeBasics/app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
+++ jetpackComposePractice/JetpackComposeBasics/app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.jetpackcomposebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
@@ -64,18 +67,26 @@ private fun MyApp(names: List<String>) {
 
 @Composable
 fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false)}
+    var expanded by rememberSaveable { mutableStateOf(false)}
+
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(color = MaterialTheme.colors.primary, modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
         Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f)) {
+            Column(modifier = Modifier.weight(1f).padding(bottom = extraPadding)) {
                 Text(text = "Hello, ")
                 Text(text = name)
             }
             OutlinedButton(
-                onClick = { expanded.value = !expanded.value }
+                onClick = { expanded = !expanded }
             ) {
-                Text( if (expanded.value) "Show less" else "Show more")
+                Text( if (expanded) "Show less" else "Show more")
             }
         }
     }
