c641f3ed70b12b58ee740a8b712ee558dae13c48
Kotaro Kamashima <kotaro0726@gmail.com>
2021-11-03 18:14:42 +0900

    Update: Add final touch

--- jetpackComposePractice/JetpackComposeBasics/app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
+++ jetpackComposePractice/JetpackComposeBasics/app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
@@ -12,10 +13,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,7 +48,7 @@ private fun MyApp(names: List<String>) {
         } else {
             LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
                 items(items = names) { name ->
-                    Greeting(name = name)
+                    CardContent(name = name)
                 }
             }
 
@@ -68,30 +73,49 @@ private fun MyApp(names: List<String>) {
 }
 
 @Composable
-fun Greeting(name: String) {
+fun CardContent(name: String) {
     var expanded by rememberSaveable { mutableStateOf(false)}
 
-    val extraPadding by animateDpAsState(
-        if (expanded) 48.dp else 0.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
-        )
-    )
+//    val extraPadding by animateDpAsState(
+//        if (expanded) 48.dp else 0.dp,
+//        animationSpec = spring(
+//            dampingRatio = Spring.DampingRatioMediumBouncy,
+//            stiffness = Spring.StiffnessLow
+//        )
+//    )
 
     Surface(color = MaterialTheme.colors.primary, modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
-        Row(modifier = Modifier.padding(24.dp)) {
-            Column(modifier = Modifier.weight(1f).padding(bottom = extraPadding)) {
+        Row(modifier = Modifier.padding(12.dp).animateContentSize(
+            animationSpec = spring(
+                dampingRatio = Spring.DampingRatioMediumBouncy,
+                stiffness = Spring.StiffnessLow
+            )
+        )) {
+            Column(modifier = Modifier
+                .weight(1f)
+                .padding(12.dp)) {
                 Text(text = "Hello, ")
                 Text(text = name, style = MaterialTheme.typography.h4.copy(
                     fontWeight = FontWeight.ExtraBold
                 ))
+                if (expanded) Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tortor risus, rutrum ac lobortis id, ullamcorper a lacus. Vestibulum lobortis.")
             }
-            OutlinedButton(
-                onClick = { expanded = !expanded }
-            ) {
-                Text( if (expanded) "Show less" else "Show more")
+            IconButton(onClick = { expanded = !expanded }) {
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = if (expanded) {
+                        stringResource(R.string.show_less)
+                    } else {
+                        stringResource(R.string.show_more)
+                    }
+
+                )
             }
+//            OutlinedButton(
+//                onClick = { expanded = !expanded }
+//            ) {
+//                Text( if (expanded) "Show less" else "Show more")
+//            }
         }
     }
 }
