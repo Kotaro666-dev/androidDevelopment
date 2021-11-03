f9864c7f2877578295ddb639318c9fcdfdd8e6d7
Kotaro Kamashima <kotaro0726@gmail.com>
2021-11-03 17:43:53 +0900

    Update: State hoisting

--- jetpackComposePractice/JetpackComposeBasics/app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
+++ jetpackComposePractice/JetpackComposeBasics/app/src/main/java/com/example/jetpackcomposebasics/MainActivity.kt
@@ -5,13 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Column
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,19 +27,31 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun MyApp(names: List<String>) {
+    var shouldShowOnBoarding by remember { mutableStateOf(true) }
     Surface(color = MaterialTheme.colors.background) {
-        Greeting("Android")
-        Column {
-            for (name in names) {
-                Greeting(name = name)
+        if (shouldShowOnBoarding) {
+            OnBoardingScreen(onClickAction = {shouldShowOnBoarding = false})
+        } else {
+            Greeting("Android")
+            Column {
+                for (name in names) {
+                    Greeting(name = name)
+                }
+                Button(
+                    onClick = { shouldShowOnBoarding = true },
+                ) {
+                    Text("Go back")
+                }
             }
         }
+
+//
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    var expanded = remember { mutableStateOf(false)}
+    val expanded = remember { mutableStateOf(false)}
 
     Surface(color = MaterialTheme.colors.primary, modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)) {
         Row(modifier = Modifier.padding(24.dp)) {
@@ -67,4 +75,29 @@ fun DefaultPreview() {
     JetpackComposeBasicsTheme {
         MyApp(names = names)
     }
+}
+
+@Composable
+fun OnBoardingScreen(onClickAction: () -> Unit) {
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onClickAction
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnBoardingPreview() = JetpackComposeBasicsTheme {
+    OnBoardingScreen(onClickAction = {})
 }
