5c047933f87e02a74862883e9e00fe5f5ebfd9f2
Kotaro Kamashima <kotaro0726@gmail.com>
2021-11-04 20:39:22 +0900

    Update: Add livedata

---
+++ AndroidKotlinFundamentals/LiveData/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -0,0 +1,86 @@
+package com.example.android.guesstheword.screens.game
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GameViewModel : ViewModel() {
+    // The current word
+    var word = MutableLiveData<String>()
+
+    // The current score
+    var score = MutableLiveData<Int>()
+
+    // The list of words - the front of the list is the next word to guess
+    private lateinit var wordList: MutableList<String>
+
+    init {
+        Log.i("GameViewModel", "GameViewModel created")
+        word.value = ""
+        score.value = 0
+        resetList()
+        nextWord()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        Log.i("GameViewModel", "GameViewModel destroyed")
+    }
+
+    /**
+     * Moves to the next word in the list
+     */
+    private fun nextWord() {
+        if (wordList.isNotEmpty()) {
+            //Select and remove a word from the list
+            word.value = wordList.removeAt(0)
+        }
+    }
+    
+
+    /**
+     * Resets the list of words and randomizes the order
+     */
+    private fun resetList() {
+        wordList = mutableListOf(
+            "queen",
+            "hospital",
+            "basketball",
+            "cat",
+            "change",
+            "snail",
+            "soup",
+            "calendar",
+            "sad",
+            "desk",
+            "guitar",
+            "home",
+            "railway",
+            "zebra",
+            "jelly",
+            "car",
+            "crow",
+            "trade",
+            "bag",
+            "roll",
+            "bubble"
+        )
+        wordList.shuffle()
+    }
+
+    fun isListEmpty(): Boolean {
+        return wordList.isEmpty()
+    }
+
+    /** Methods for buttons presses **/
+
+    fun onSkip() {
+        score.value = (score.value)?.minus(1)
+        nextWord()
+    }
+
+    fun onCorrect() {
+        score.value = (score.value)?.plus(1)
+        nextWord()
+    }
+}
