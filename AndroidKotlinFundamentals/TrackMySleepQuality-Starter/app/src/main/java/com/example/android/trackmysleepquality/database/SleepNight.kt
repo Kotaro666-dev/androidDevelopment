ac57cabc825cb19dfaee84fd94cfe358bea9de8d
Kotaro Kamashima <kotaro0726@gmail.com>
2021-11-05 08:43:57 +0900

    Fix: Fix format for data class

--- AndroidKotlinFundamentals/TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
+++ AndroidKotlinFundamentals/TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -21,17 +21,17 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "daily_sleep_quality_table")
-data class SleepNight {
+data class SleepNight(
     @PrimaryKey(autoGenerate = true)
-    var nightId: Long = 0L
+    var nightId: Long = 0L,
 
     @ColumnInfo(name = "start_time_milli")
-    private val startTimeMilli: Long = System.currentTimeMillis()
+    private val startTimeMilli: Long = System.currentTimeMillis(),
 
     @ColumnInfo(name = "end_time_milli")
-    var endTimeMilli: Long = startTimeMilli
+    var endTimeMilli: Long = startTimeMilli,
 
     @ColumnInfo(name = "quality_rating")
     var sleepQuality: Int = -1
-}
+)
 
