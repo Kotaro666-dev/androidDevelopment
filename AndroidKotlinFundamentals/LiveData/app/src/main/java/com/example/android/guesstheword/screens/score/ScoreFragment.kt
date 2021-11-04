367c2275a6dd3188b8aa7772eff3b2d267de42c9
Kotaro Kamashima <kotaro0726@gmail.com>
2021-11-04 23:53:58 +0900

    Update: Use LiveData to ScoreViewModel

--- AndroidKotlinFundamentals/LiveData/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ AndroidKotlinFundamentals/LiveData/app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -24,6 +24,7 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.android.guesstheword.R
 import com.example.android.guesstheword.databinding.ScoreFragmentBinding
@@ -56,7 +57,13 @@ class ScoreFragment : Fragment() {
         viewModel.score.observe(viewLifecycleOwner, Observer { newScore ->
             binding.scoreText.text = newScore.toString()
         })
-
+        viewModel.eventPlayAgain.observe(viewLifecycleOwner, Observer { playAgain ->
+            if (playAgain) {
+                findNavController().navigate(ScoreFragmentDirections.actionRestart())
+                viewModel.onPlayAgainComplete()
+            }
+        })
+        binding.playAgainButton.setOnClickListener { viewModel.onPlayAgain() }
 
         return binding.root
     }
