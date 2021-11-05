7d2c35bad3f3531221bebdb205d2b6ed4fea8c65
Kotaro Kamashima <kotaro0726@gmail.com>
2021-11-05 09:00:53 +0900

    Update: Add Room instance

--- AndroidKotlinFundamentals/TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
+++ AndroidKotlinFundamentals/TrackMySleepQuality-Starter/app/src/main/java/com/example/android/trackmysleepquality/database/SleepNight.kt
@@ -26,7 +26,7 @@ data class SleepNight(
     var nightId: Long = 0L,
 
     @ColumnInfo(name = "start_time_milli")
-    private val startTimeMilli: Long = System.currentTimeMillis(),
+    val startTimeMilli: Long = System.currentTimeMillis(),
 
     @ColumnInfo(name = "end_time_milli")
     var endTimeMilli: Long = startTimeMilli,
