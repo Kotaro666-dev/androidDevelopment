7219baf14545e9d500a339612e3f3024433c670c
Kotaro Kamashima <kotaro0726@gmail.com>
2021-11-06 12:29:12 +0900

    Update: Add view binding and button to display webview

--- mockups/WebviewMockup/app/src/main/java/com/example/webviewmockup/MainActivity.kt
+++ mockups/WebviewMockup/app/src/main/java/com/example/webviewmockup/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.webviewmockup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.webviewmockup.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        binding.webViewButton.setOnClickListener { onTapButton() }
+        setContentView(view)
+    }
+
+    private fun onTapButton() {
+        // Open WebView
     }
 }
