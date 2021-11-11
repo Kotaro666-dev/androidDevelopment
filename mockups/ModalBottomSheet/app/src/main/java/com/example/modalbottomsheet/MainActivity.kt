b98ca28fa92cbf9bac417763b16411cfabbb24d6
Kotaro Kamashima <kotaro0726@gmail.com>
2021-11-12 08:46:35 +0900

    Update: Display dialog on main activity

--- mockups/ModalBottomSheet/app/src/main/java/com/example/modalbottomsheet/MainActivity.kt
+++ mockups/ModalBottomSheet/app/src/main/java/com/example/modalbottomsheet/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.example.modalbottomsheet
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.modalbottomsheet.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        binding.displayBottomSheetButton.setOnClickListener { onClickDisplayBottomSheetButton() }
+
+        setContentView(view)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    private fun onClickDisplayBottomSheetButton() {
+        supportFragmentManager.let {
+            MyBottomSheetDialogFragment.newInstance(Bundle()).apply {
+                show(it, tag)
+            }
+        }
     }
 }
---
+++ mockups/ModalBottomSheet/app/src/main/java/com/example/modalbottomsheet/MyBottomSheetDialogFragment.kt
@@ -0,0 +1,30 @@
+package com.example.modalbottomsheet
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class MyBottomSheetDialogFragment : BottomSheetDialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_bottom_sheet_dialog, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(bundle: Bundle): MyBottomSheetDialogFragment {
+            val fragment = MyBottomSheetDialogFragment()
+            fragment.arguments = bundle
+            return fragment
+        }
+    }
+}
