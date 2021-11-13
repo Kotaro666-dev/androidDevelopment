39cb583465ad853c00e2fc8b96961ce0860d8540
Kotaro Kamashima <kotaro0726@gmail.com>
2021-11-13 16:17:21 +0900

    Update: Add home screen

--- mockups/BottomNavigationBarWithSaveFragmentInstanceState/app/src/main/java/com/example/bottomnavigationbarwithsavefragmentinstancestate/MainActivity.kt
+++ mockups/BottomNavigationBarWithSaveFragmentInstanceState/app/src/main/java/com/example/bottomnavigationbarwithsavefragmentinstancestate/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.bottomnavigationbarwithsavefragmentinstancestate
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.bottomnavigationbarwithsavefragmentinstancestate.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
