deb41cc5660ea844a32215c32957ad3da9c57c68
Kotaro Kamashima <kotaro0726@gmail.com>
2021-11-14 12:31:27 +0900

    Update: Add ViewPager2 dependency

--- mockups/BottomNavigationBarWithSaveFragmentInstanceState/app/src/main/java/com/example/bottomnavigationbarwithsavefragmentinstancestate/HomeFragment.kt
+++ mockups/BottomNavigationBarWithSaveFragmentInstanceState/app/src/main/java/com/example/bottomnavigationbarwithsavefragmentinstancestate/HomeFragment.kt
@@ -1,9 +1,11 @@
 package com.example.bottomnavigationbarwithsavefragmentinstancestate
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.bottomnavigationbarwithsavefragmentinstancestate.databinding.FragmentHomeBinding
 
@@ -11,6 +13,18 @@ class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i("HomeFragment", "Initialize Fragment")
+        if (savedInstanceState != null) {
+            //Restore the fragment's state here
+            Log.i("HomeFragment", "Restore fragment's state")
+
+            val data = savedInstanceState.getString("HOME")
+            Toast.makeText(context, data, Toast.LENGTH_SHORT).show()
+        }
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,8 +36,19 @@ class HomeFragment : Fragment() {
         return view
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        //Save the fragment's state here
+        Log.i("HomeFragment", "Saving fragment's state")
+
+        outState.putString("HOME", "This is Home")
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
+
+        Log.i("HomeFragment", "Destroy Fragment")
         _binding = null
     }
 }
