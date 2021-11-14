572160a7466d74a4fede5e039eda7742c0e45be9
Kotaro Kamashima <kotaro0726@gmail.com>
2021-11-14 12:46:20 +0900

    Update: Add function to swipe pages

--- mockups/ViewPager2WithDotsIndicator/app/src/main/java/com/example/viewpager2withdotsindicator/MainActivity.kt
+++ mockups/ViewPager2WithDotsIndicator/app/src/main/java/com/example/viewpager2withdotsindicator/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.viewpager2withdotsindicator
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.viewpager2withdotsindicator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        val pagerAdaptor = ViewPagerAdapter(this)
+        binding.viewPager2.adapter = pagerAdaptor
+        setContentView(view)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
---
+++ mockups/ViewPager2WithDotsIndicator/app/src/main/java/com/example/viewpager2withdotsindicator/ViewPagerAdaptor.kt
@@ -0,0 +1,17 @@
+package com.example.viewpager2withdotsindicator
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
+    override fun getItemCount(): Int = 3
+
+    override fun createFragment(position: Int): Fragment =
+        when (position) {
+            0 -> FirstFragment()
+            1 -> SecondFragment()
+            2 -> ThirdFragment()
+            else -> FirstFragment()
+        }
+}
