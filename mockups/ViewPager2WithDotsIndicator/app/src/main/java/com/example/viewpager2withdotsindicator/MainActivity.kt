1b34f9be79a4fd27b1f571ccf57e5f518cd7c2f3
Kotaro Kamashima <kotaro0726@gmail.com>
2021-11-18 22:32:00 +0900

    Update: Connect ViewPagaer2 with Tabs

--- mockups/ViewPager2WithDotsIndicator/app/src/main/java/com/example/viewpager2withdotsindicator/MainActivity.kt
+++ mockups/ViewPager2WithDotsIndicator/app/src/main/java/com/example/viewpager2withdotsindicator/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.viewpager2withdotsindicator
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.viewpager2withdotsindicator.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +17,12 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
 
         val pagerAdaptor = ViewPagerAdapter(this)
-        binding.viewPager2.adapter = pagerAdaptor
+        val tabLayout = binding.tabLayout
+        val viewPager = binding.viewPager2
+        viewPager.adapter = pagerAdaptor
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            println("$tab: $position")
+        }.attach()
         setContentView(view)
     }
 
