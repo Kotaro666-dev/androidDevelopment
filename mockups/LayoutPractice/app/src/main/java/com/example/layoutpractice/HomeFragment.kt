92ae6c2968070a4b4e5d60b16ee0aa105aac2757
Kotaro Kamashima <kotaro0726@gmail.com>
2021-11-20 12:38:28 +0900

    Update: Add TextButton and edit layout

--- mockups/LayoutPractice/app/src/main/java/com/example/layoutpractice/HomeFragment.kt
+++ mockups/LayoutPractice/app/src/main/java/com/example/layoutpractice/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.layoutpractice.databinding.FragmentHomeBinding
 
@@ -18,11 +19,28 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        binding.outlinedButton.setOnClickListener { onClickOutlinedButton() }
+        binding.filledButton.setOnClickListener { onClickFilledButton() }
+        binding.textButton.setOnClickListener { onClickTextButton() }
+
         return view
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun onClickOutlinedButton() {
+        Toast.makeText(context, "onClickOutlinedButton", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun onClickFilledButton() {
+        Toast.makeText(context, "onClickFilledButton", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun onClickTextButton() {
+        Toast.makeText(context, "onClickTextButton", Toast.LENGTH_SHORT).show()
     }
 }
