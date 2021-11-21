da02af0285601a7dde214029755be75c56117888
Kotaro Kamashima <kotaro0726@gmail.com>
2021-11-21 18:22:31 +0900

    Update: Share data between Activities

--- mockups/ConcatAdaptors/app/src/main/java/com/example/concatadaptors/fragments/ItemPageFragment.kt
+++ mockups/ConcatAdaptors/app/src/main/java/com/example/concatadaptors/fragments/ItemPageFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.concatadaptors.R
 import com.example.concatadaptors.databinding.FragmentItemPageBinding
+import com.example.concatadaptors.helpers.EXTRA_ITEM_TEXT
+
 
 class ItemPageFragment : Fragment() {
     private var _binding: FragmentItemPageBinding? = null
@@ -17,8 +20,14 @@ class ItemPageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentItemPageBinding.inflate(layoutInflater)
-
         val view = binding.root
+
+        val intent = activity?.intent
+        if (intent != null) {
+            val itemText = intent.getStringExtra(EXTRA_ITEM_TEXT)
+            binding.itemImageView.setImageResource(R.mipmap.ic_launcher)
+            binding.itemTextView.text = itemText
+        }
         return view
     }
 
--- mockups/ConcatAdaptors/app/src/main/java/com/example/concatadaptors/helpers/RecyclerViewHolder.kt
+++ mockups/ConcatAdaptors/app/src/main/java/com/example/concatadaptors/helpers/RecyclerViewHolder.kt
@@ -10,6 +10,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.concatadaptors.R
 import com.example.concatadaptors.activities.ItemPageActivity
 
+const val EXTRA_ITEM_TEXT = "item_text"
+
 class RecyclerViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     val itemTextView: TextView = view.findViewById(R.id.itemListTextView)
     val itemImageView: ImageView = view.findViewById(R.id.itemListImageView)
@@ -19,7 +21,10 @@ class RecyclerViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val cardView: CardView = view.findViewById(R.id.card_view)
         cardView.setOnClickListener {
 //            Toast.makeText(view.context, itemTextView.text, Toast.LENGTH_SHORT).show()
-            val intent = Intent(view.context, ItemPageActivity::class.java)
+            val intent = Intent(view.context, ItemPageActivity::class.java).apply {
+                val itemText = itemTextView.text.toString()
+                putExtra(EXTRA_ITEM_TEXT, itemText)
+            }
             startActivity(view.context, intent, null)
         }
     }
