ad042a29fd9b2969f6c003f3a376b8f4e2985476
Kotaro Kamashima <kotaro0726@gmail.com>
2021-11-21 17:19:17 +0900

    Update: Get click event

--- mockups/ConcatAdaptors/app/src/main/java/com/example/concatadaptors/RecyclerViewHolder.kt
+++ mockups/ConcatAdaptors/app/src/main/java/com/example/concatadaptors/RecyclerViewHolder.kt
@@ -1,6 +1,8 @@
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.concatadaptors.R
 
@@ -10,6 +12,9 @@ class RecyclerViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     init {
         // Define click listener for the ViewHolder's View.
-
+        val cardView: CardView = view.findViewById(R.id.card_view)
+        cardView.setOnClickListener {
+            Toast.makeText(view.context, itemTextView.text, Toast.LENGTH_SHORT).show()
+        }
     }
 }
