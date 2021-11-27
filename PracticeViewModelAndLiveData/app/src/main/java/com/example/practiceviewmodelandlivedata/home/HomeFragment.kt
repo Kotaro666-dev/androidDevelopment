c5ea8cba24252cee44c5009c7adae09c3823b89e
Kotaro Kamashima <kotaro0726@gmail.com>
2021-11-27 17:16:26 +0900

    Update: Connect views with ViewModel and LiveData

--- PracticeViewModelAndLiveData/app/src/main/java/com/example/practiceviewmodelandlivedata/home/HomeFragment.kt
+++ PracticeViewModelAndLiveData/app/src/main/java/com/example/practiceviewmodelandlivedata/home/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.practiceviewmodelandlivedata.R
 import com.example.practiceviewmodelandlivedata.databinding.FragmentHomeBinding
 
@@ -25,6 +26,17 @@ class HomeFragment : Fragment() {
             container,
             false
         )
+
+        // HomeViewModelのインスタンス化
+        viewModel = ViewModelProvider(this)[HomeViewModel::class.java]
+        // インスタンス化したHomeViewModelを、View側の変数に参照させる
+        binding.homeViewModel = viewModel
+        // Sets the LifecycleOwner that should be used for observing changes of LiveData in this binding.
+        // If a LiveData is in one of the binding expressions and no LifecycleOwner is set,
+        // the LiveData will not be observed and updates to it will not be propagated to the UI.
+        // 以下の処理を行わないと、LiveData が View に反映されない
+        binding.lifecycleOwner = viewLifecycleOwner
+
         return binding.root
     }
 }
--- PracticeViewModelAndLiveData/app/src/main/java/com/example/practiceviewmodelandlivedata/home/HomeViewModel.kt
+++ PracticeViewModelAndLiveData/app/src/main/java/com/example/practiceviewmodelandlivedata/home/HomeViewModel.kt
@@ -1,10 +1,25 @@
 package com.example.practiceviewmodelandlivedata.home
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class HomeViewModel : ViewModel() {
+    private val _score = MutableLiveData<Int>()
+    val score: LiveData<Int> get() = _score
 
     init {
+        _score.value = 0
+    }
 
+    fun onTapPlus() {
+        _score.value = (score.value)?.plus(1)
+    }
+
+    fun onTapMinus() {
+        if (_score.value == 0) {
+            return
+        }
+        _score.value = (score.value)?.minus(1)
     }
 }
