252aadffefb23b9f397ff71db3c51a1219c3b6d1
Kotaro Kamashima <kotaro0726@gmail.com>
2021-12-05 16:21:34 +0900

    Update: Add method to access to Database

--- mockups/MvvmUsecaseCoordinatorRepositoryPractice/app/src/main/java/com/example/mvvmusecasecoordinatorrepositorypractice/Repository.kt
+++ mockups/MvvmUsecaseCoordinatorRepositoryPractice/app/src/main/java/com/example/mvvmusecasecoordinatorrepositorypractice/Repository.kt
@@ -1,7 +1,16 @@
 package com.example.mvvmusecasecoordinatorrepositorypractice
 
+import androidx.lifecycle.LiveData
 import com.example.mvvmusecasecoordinatorrepositorypractice.database.AppDatabase
+import com.example.mvvmusecasecoordinatorrepositorypractice.database.TodoDataClass
 
-class Repository(val todoDatabase: AppDatabase) {
 
+class Repository(private val database: AppDatabase) {
+    fun getTodoList(): LiveData<List<TodoDataClass>> {
+        return database.todoDatabaseDao().getAllTodos()
+    }
+
+    fun addTodo(todo: TodoDataClass) {
+        database.todoDatabaseDao().insert(todo)
+    }
 }
