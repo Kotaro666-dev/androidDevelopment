3c62149f2f30bd154457f77a1be5a926982ab478
Kotaro Kamashima <kotaro0726@gmail.com>
2021-12-05 16:23:25 +0900

    Update: Add RecyclerView and display test case

--- mockups/MvvmUsecaseCoordinatorRepositoryPractice/app/src/main/java/com/example/mvvmusecasecoordinatorrepositorypractice/home/HomeActivity.kt
+++ mockups/MvvmUsecaseCoordinatorRepositoryPractice/app/src/main/java/com/example/mvvmusecasecoordinatorrepositorypractice/home/HomeActivity.kt
@@ -3,7 +3,9 @@ package com.example.mvvmusecasecoordinatorrepositorypractice.home
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.mvvmusecasecoordinatorrepositorypractice.R
+import com.example.mvvmusecasecoordinatorrepositorypractice.database.TodoDataClass
 import com.example.mvvmusecasecoordinatorrepositorypractice.databinding.ActivityHomeBinding
 import com.example.mvvmusecasecoordinatorrepositorypractice.model.ViewModels
 
@@ -16,6 +18,8 @@ class HomeActivity : AppCompatActivity() {
         ViewModels.of(this, HomeViewModel::class.java)
     }
 
+    private lateinit var mAdapter: CustomAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.lifecycleOwner = this
@@ -24,5 +28,21 @@ class HomeActivity : AppCompatActivity() {
         binding.buttonGoToAddTodo.setOnClickListener {
             viewModel.goToAddTodoScreen(this)
         }
+
+        val dataSet = listOf(
+            TodoDataClass(title = "test1"),
+            TodoDataClass(title = "test2"),
+            TodoDataClass(title = "test3")
+        )
+
+        // RecyclerViewの取得
+        val recyclerView = binding.recyclerView
+
+        // LayoutManagerの設定
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        // CustomAdapterの生成と設定
+        mAdapter = CustomAdapter(dataSet)
+        recyclerView.adapter = mAdapter
     }
 }
