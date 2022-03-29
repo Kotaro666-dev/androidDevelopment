ca7543db5792936bf036e7a434a24cb1aa76b449
Kotaro Kamashima <kotaro0726@gmail.com>
2022-03-29 10:09:43 +0900

    Update: Add SearchBar on SearchPage.kt

--- MercariInJetpackCompose/app/src/main/java/com/example/mercariinjetpackcompose/views/SearchPage.kt
+++ MercariInJetpackCompose/app/src/main/java/com/example/mercariinjetpackcompose/views/SearchPage.kt
@@ -1,24 +1,72 @@
 package com.example.mercariinjetpackcompose.views
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
 @Composable
 fun SearchPage(navController: NavHostController) {
-    Scaffold {
+    Scaffold(
+        topBar = { SearchBar(navController = navController) }
+    ) {
         Column(
             modifier = Modifier.fillMaxSize(),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text("検索画面")
+        }
+    }
+}
+
+@Composable
+fun SearchBar(navController: NavHostController) {
+    TopAppBar {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(50.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            IconButton(onClick = {
+                navController.popBackStack()
+            }) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "Go back",
+                    modifier = Modifier
+                        .size(30.dp)
+                )
+            }
+            Spacer(modifier = Modifier.width(20.dp))
+            TextField(
+                modifier = Modifier.fillMaxWidth(),
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "Go back",
+                        modifier = Modifier
+                            .size(30.dp)
+                    )
+                },
+                placeholder = { Text("キーワードからさがす") },
+                colors = TextFieldDefaults.textFieldColors(
+                    backgroundColor = Color.Transparent,
+                    unfocusedIndicatorColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent
+                ),
+                maxLines = 1,
+                value = "",
+                onValueChange = { /*TODO */ },
+            )
         }
     }
 }
