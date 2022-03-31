3242ac0b8383c6badd4bdf387111918d95071f83
Kotaro Kamashima <kotaro0726@gmail.com>
2022-03-31 13:57:02 +0900

    Update: Add Textfield focus feature

--- MercariInJetpackCompose/app/src/main/java/com/example/mercariinjetpackcompose/views/SearchPage.kt
+++ MercariInJetpackCompose/app/src/main/java/com/example/mercariinjetpackcompose/views/SearchPage.kt
@@ -1,21 +1,47 @@
 package com.example.mercariinjetpackcompose.views
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
+@ExperimentalComposeUiApi
 @Composable
 fun SearchPage(navController: NavHostController) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val focusManager = LocalFocusManager.current
+
+    DisposableEffect(Unit) {
+        onDispose {
+            keyboardController?.hide()
+            focusManager.clearFocus(true)
+        }
+    }
+
     Scaffold(
-        topBar = { SearchBar(navController = navController) }
+        topBar = { SearchBar(navController = navController) },
+        modifier = Modifier.clickable(
+            interactionSource = interactionSource,
+            indication = null,
+        ) {
+            keyboardController?.hide()
+            focusManager.clearFocus(true)
+        }
     ) {
         Column(
             modifier = Modifier.fillMaxSize(),
@@ -27,8 +53,16 @@ fun SearchPage(navController: NavHostController) {
     }
 }
 
+@ExperimentalComposeUiApi
 @Composable
 fun SearchBar(navController: NavHostController) {
+    var text by remember { mutableStateOf("") }
+    val focusRequester = remember { FocusRequester() }
+
+    LaunchedEffect(Unit) {
+        focusRequester.requestFocus()
+    }
+
     TopAppBar {
         Row(
             modifier = Modifier
@@ -48,11 +82,13 @@ fun SearchBar(navController: NavHostController) {
             }
             Spacer(modifier = Modifier.width(20.dp))
             TextField(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .focusRequester(focusRequester),
                 leadingIcon = {
                     Icon(
                         imageVector = Icons.Default.Search,
-                        contentDescription = "Go back",
+                        contentDescription = "Search",
                         modifier = Modifier
                             .size(30.dp)
                     )
@@ -60,12 +96,13 @@ fun SearchBar(navController: NavHostController) {
                 placeholder = { Text("キーワードからさがす") },
                 colors = TextFieldDefaults.textFieldColors(
                     backgroundColor = Color.Transparent,
+                    focusedIndicatorColor = Color.Transparent,
                     unfocusedIndicatorColor = Color.Transparent,
-                    focusedIndicatorColor = Color.Transparent
+                    cursorColor = Color.Blue,
                 ),
                 maxLines = 1,
-                value = "",
-                onValueChange = { /*TODO */ },
+                value = text,
+                onValueChange = { newValue -> text = newValue },
             )
         }
     }
