887f6d24e369a6160b40354b37d1ad62bfb40120
Kotaro Kamashima <kotaro0726@gmail.com>
2022-11-27 12:57:15 +0900

    Update: Add ranking and setting modules

---
+++ PracticeMultiModule2/ui/ranking/src/main/java/com/example/ranking/RankingFragment.kt
@@ -0,0 +1,17 @@
+package com.example.ranking
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class RankingFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_ranking, container, false)
+    }
+}
