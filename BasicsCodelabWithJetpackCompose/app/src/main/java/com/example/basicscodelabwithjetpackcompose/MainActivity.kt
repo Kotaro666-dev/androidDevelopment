5c38c51dc3860ff4d05e894a2ad52038eda9fd27
釜島 光太郎 <k_kamashima@MacBook-Pro-2.local>
2023-03-30 12:20:33 +0900

    Update: Customize UI design on Greeting func

--- BasicsCodelabWithJetpackCompose/app/src/main/java/com/example/basicscodelabwithjetpackcompose/MainActivity.kt
+++ BasicsCodelabWithJetpackCompose/app/src/main/java/com/example/basicscodelabwithjetpackcompose/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.basicscodelabwithjetpackcompose.ui.theme.BasicsCodelabWithJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +20,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             BasicsCodelabWithJetpackComposeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
                     Greeting("Android")
                 }
             }
@@ -28,7 +33,9 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Surface(color = MaterialTheme.colorScheme.primary) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(all = 24.dp))
+    }
 }
 
 @Preview(showBackground = true)
