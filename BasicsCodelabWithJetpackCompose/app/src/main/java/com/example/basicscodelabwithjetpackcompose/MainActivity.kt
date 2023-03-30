7a3aa5ceb9d2d0949a77b227ede315afdb72cb18
釜島 光太郎 <k_kamashima@MacBook-Pro-2.local>
2023-03-30 12:38:25 +0900

    Update: Add Column

--- BasicsCodelabWithJetpackCompose/app/src/main/java/com/example/basicscodelabwithjetpackcompose/MainActivity.kt
+++ BasicsCodelabWithJetpackCompose/app/src/main/java/com/example/basicscodelabwithjetpackcompose/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.basicscodelabwithjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -26,19 +28,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-private fun MyApp(modifier: Modifier = Modifier) {
+private fun MyApp(modifier: Modifier = Modifier, names: List<String> = listOf("World", "Compose")) {
     Surface(
         modifier = modifier,
         color = MaterialTheme.colorScheme.background,
     ) {
-        Greeting(name = "Android")
+        Column(modifier = modifier.padding(vertical = 4.dp)) {
+            for (name in names) {
+                Greeting(name = name)
+            }
+        }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colorScheme.primary) {
-        Text(text = "Hello $name!", modifier = Modifier.padding(all = 24.dp))
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(24.dp)
+        ) {
+            Text(text = "Hello ")
+            Text(text = name)
+        }
     }
 }
 
