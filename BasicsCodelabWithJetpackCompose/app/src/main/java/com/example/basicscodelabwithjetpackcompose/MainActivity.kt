ae6049b319798605a3d81bd6467b7419f8690267
Kotaro Kamashima <kotaro0726@gmail.com>
2023-03-30 14:16:52 +0900

    Update: Customize theme

--- BasicsCodelabWithJetpackCompose/app/src/main/java/com/example/basicscodelabwithjetpackcompose/MainActivity.kt
+++ BasicsCodelabWithJetpackCompose/app/src/main/java/com/example/basicscodelabwithjetpackcompose/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.basicscodelabwithjetpackcompose
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -17,6 +18,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
@@ -121,7 +123,10 @@ fun Greeting(name: String) {
                     .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello ")
-                Text(text = name)
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.Bold)
+                )
             }
             ElevatedButton(onClick = { expanded.value = !expanded.value }) {
                 Text(if (expanded.value) "Show less" else "Show more")
@@ -130,7 +135,8 @@ fun Greeting(name: String) {
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320, uiMode = UI_MODE_NIGHT_YES, name = "Dark Mode")
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     BasicsCodelabWithJetpackComposeTheme {
