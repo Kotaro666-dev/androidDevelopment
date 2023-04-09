4d9968cd902ec7c142e881b32f53afe3c07d23d1
Kotaro Kamashima <kotaro0726@gmail.com>
2023-04-09 11:12:21 +0900

    Update: Implement CounterPresenterImpl class with empty method bodies

---
+++ CounterAppWithMVPPattern/app/src/main/java/com/example/counterappwithmvppattern/presenter/CounterPresenterImpl.kt
@@ -0,0 +1,19 @@
+package com.example.counterappwithmvppattern.presenter
+
+class CounterPresenterImpl : CounterPresenter {
+    override fun loadCounter() {
+        TODO("Not yet implemented")
+        // Access Repository to get the counter
+        // Then, call showValue() on the View
+    }
+
+    override fun onIncrementButtonClicked() {
+        TODO("Not yet implemented")
+        // Access Repository to increment the counter
+    }
+
+    override fun onDecrementButtonClicked() {
+        TODO("Not yet implemented")
+        // Access Repository to decrement the counter
+    }
+}
