443b394b0b7933cdb304dc2b1c18679fe9a1112e
Kotaro Kamashima <kotaro0726@gmail.com>
2023-04-09 19:14:11 +0900

    Update: Inject presenter to view

--- CounterAppWithMVPPattern/app/src/main/java/com/example/counterappwithmvppattern/ui/MainActivity.kt
+++ CounterAppWithMVPPattern/app/src/main/java/com/example/counterappwithmvppattern/ui/MainActivity.kt
@@ -3,24 +3,31 @@ package com.example.counterappwithmvppattern.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.counterappwithmvppattern.databinding.ActivityMainBinding
+import com.example.counterappwithmvppattern.presenter.CounterPresenter
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity(), CounterView {
     private lateinit var binding: ActivityMainBinding
+
+    @Inject
+    lateinit var presenter: CounterPresenter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        // TODO: Presenter should be called here to initialize the view
+        presenter.loadCounter()
 
         binding.incrementButton.setOnClickListener {
             println("Increment button clicked")
-            // TODO: Presenter should be called here
+            presenter.onIncrementButtonClicked()
         }
 
         binding.decrementButton.setOnClickListener {
             println("Decrement button clicked")
-            // TODO: Presenter should be called here
+            presenter.onDecrementButtonClicked()
         }
     }
 
