f299db9ae926aa455348ce937936bca6f29ea5bb
Kotaro Kamashima <kotaro0726@gmail.com>
2023-04-09 11:14:03 +0900

    Update: Implement CounterView and add TODO comments

--- CounterAppWithMVPPattern/app/src/main/java/com/example/counterappwithmvppattern/ui/MainActivity.kt
+++ CounterAppWithMVPPattern/app/src/main/java/com/example/counterappwithmvppattern/ui/MainActivity.kt
@@ -1,24 +1,30 @@
-package com.example.counterappwithmvppattern
+package com.example.counterappwithmvppattern.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.counterappwithmvppattern.databinding.ActivityMainBinding
 
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity : AppCompatActivity(), CounterView {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        // TODO: Presenter should be called here to initialize the view
 
         binding.incrementButton.setOnClickListener {
             println("Increment button clicked")
+            // TODO: Presenter should be called here
         }
 
         binding.decrementButton.setOnClickListener {
             println("Decrement button clicked")
+            // TODO: Presenter should be called here
         }
+    }
+
+    override fun showValue(value: Int) {
+        binding.counterValue.text = value.toString()
     }
 }
