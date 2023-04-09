482383081572959e40a57804d626d384ca703260
Kotaro Kamashima <kotaro0726@gmail.com>
2023-04-09 10:09:03 +0900

    Update: Add viewBinding and bind view with Activity

--- CounterAppWithMVPPattern/app/src/main/java/com/example/counterappwithmvppattern/MainActivity.kt
+++ CounterAppWithMVPPattern/app/src/main/java/com/example/counterappwithmvppattern/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.counterappwithmvppattern
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.counterappwithmvppattern.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.incrementButton.setOnClickListener {
+            println("Increment button clicked")
+        }
+
+        binding.decrementButton.setOnClickListener {
+            println("Decrement button clicked")
+        }
     }
 }
