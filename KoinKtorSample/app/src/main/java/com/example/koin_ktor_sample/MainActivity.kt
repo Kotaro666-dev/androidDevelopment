98b591a9da52428f44fa4c2774b70de123aa6e70
Kotaro Kamashima <kotaro0726@gmail.com>
2023-08-05 15:20:47 +0900

    Update: Initialize project

---
+++ KoinKtorSample/app/src/main/java/com/example/koin_ktor_sample/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.koin_ktor_sample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.koin_ktor_sample.ui.theme.Koin_ktor_sampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Koin_ktor_sampleTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Koin_ktor_sampleTheme {
+        Greeting("Android")
+    }
+}
